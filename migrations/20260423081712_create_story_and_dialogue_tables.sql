cc264972b60efcd25b944a72662bd67bcdd91475
changyc9928 <changyc9928@gmail.com>
2026-04-24 17:52:04 +0800

    Added scripts

---
+++ migrations/20260423081712_create_story_and_dialogue_tables.sql
@@ -0,0 +1,16 @@
+-- Add migration script here
+CREATE TABLE story (
+    summary TEXT NOT NULL,
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    PRIMARY KEY (updated_at)
+);
+
+CREATE TABLE dialogue (
+    dialogue TEXT NOT NULL,
+    author_name TEXT NOT NULL,
+    author_character TEXT NOT NULL,
+    author_discord_id TEXT NOT NULL,
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+
+    PRIMARY KEY (updated_at)
+);
